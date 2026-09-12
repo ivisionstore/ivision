db5cf496f96e2516262239e2c41296e9c2396a3f
ivisionstore <allseassonstore@gmail.com>
2026-09-12 13:10:32 +0300

    style: improve coupon page spacing for logo

--- app/coupon/layout.tsx
+++ app/coupon/layout.tsx
@@ -7,6 +7,6 @@ export default function CouponLayout({ children }: { children: ReactNode }) {
     <header className="absolute top-0 left-0 right-0 z-20 px-5 py-5 sm:px-8 sm:py-7 flex justify-center lg:justify-start">
       <img src="/ivision-logo.svg" alt="IVISION" className="w-36 sm:w-44 h-auto" />
     </header>
-    <div className="relative z-10">{children}</div>
+    <div className="relative z-10 pt-20">{children}</div>
   </div>
 }
